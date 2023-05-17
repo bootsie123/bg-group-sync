0a9bb61278b1c591c73a3dd1a953fb029f737033
John Arrandale <bootsie227@gmail.com>
2023-05-17 12:22:03 -0400

    Changed FindUser to use a generic query instead of full name

--- src/functions/google/googleFindUser.ts
+++ src/functions/google/googleFindUser.ts
@@ -11,12 +11,12 @@ export const FUNCTION_NAME = "googleFindUser";
 
 /**
  * Finds a user under the Google domain using their full name
- * @param fullName The concatenated first name and last name of the user to find
+ * @param query The query string to use to find the user
  * @param context The invocation context for the function
  * @returns The {@link adminDirectoryV1.Schema$User} user if found, otherwise undefined
  */
 export async function googleFindUser(
-  fullName: string,
+  query: string,
   context: InvocationContext
 ): Promise<adminDirectoryV1.Schema$User> {
   const logger = new Logger(context, "Google");
@@ -27,20 +27,20 @@ export async function googleFindUser(
     const userList = await google.listUsers({
       domain: environment.google.domain,
       maxResults: 2,
-      query: `name='${fullName}'`
+      query
     });
 
     if (!userList.users) {
       return undefined;
     } else if (userList.users.length > 1) {
-      logger.log(Severity.Warning, `Multiple Google accounts found for user ${fullName}`);
+      logger.log(Severity.Warning, `Multiple Google accounts found with query ${query}`);
 
       return undefined;
     }
 
     return userList.users[0];
   } catch (err) {
-    logger.log(Severity.Error, err, "\nInput Parameters:", fullName);
+    logger.log(Severity.Error, err, "\nInput Parameters:", query);
 
     throw err;
   }
