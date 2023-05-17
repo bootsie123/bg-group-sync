0cc8db11d69e8a4629b15b69657eeace4a199d15
John Arrandale <bootsie227@gmail.com>
2023-05-17 15:52:33 -0400

    Reformatted environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,34 +1,36 @@
 export default {
-  production: process.env.APPSETTING_NODE_ENV ? true : process.env.NODE_ENV === "production",
+  production: process.env.APPSETTING_FUNCTIONS_WORKER_RUNTIME
+    ? true
+    : process.env.NODE_ENV === "production",
   sync: {
-    schedule: process.env["SYNC_SCHEDULE"] || "0 0 0 * * *",
-    scheduleEnabled: process.env["SYNC_SCHEDULE_ENABLED"] === "true"
+    schedule: process.env.SYNC_SCHEDULE || "0 0 0 * * *",
+    scheduleEnabled: process.env.SYNC_SCHEDULE_ENABLED === "true"
   },
   blackbaud: {
     oauth: {
-      id: process.env["BLACKBAUD_OAUTH_ID"],
-      secret: process.env["BLACKBAUD_OAUTH_SECRET"],
-      tokenHost: process.env["BLACKBAUD_OAUTH_TOKEN_HOST"] || "https://oauth2.sky.blackbaud.com",
-      authorizePath: process.env["BLACKBAUD_OAUTH_AUTHORIZE_PATH"] || "/authorization",
-      tokenPath: process.env["BLACKBAUD_OAUTH_TOKEN_PATH"] || "/token"
+      id: process.env.BLACKBAUD_OAUTH_ID,
+      secret: process.env.BLACKBAUD_OAUTH_SECRET,
+      tokenHost: process.env.BLACKBAUD_OAUTH_TOKEN_HOST || "https://oauth2.sky.blackbaud.com",
+      authorizePath: process.env.BLACKBAUD_OAUTH_AUTHORIZE_PATH || "/authorization",
+      tokenPath: process.env.BLACKBAUD_OAUTH_TOKEN_PATH || "/token"
     },
     sky: {
-      skyAPIEndpoint: process.env["BLACKBAUD_SKY_API_ENDPOINT"] || "https://api.sky.blackbaud.com",
-      subscriptionKey: process.env["BLACKBAUD_SUBSCRIPTION_KEY"]
+      skyAPIEndpoint: process.env.BLACKBAUD_SKY_API_ENDPOINT || "https://api.sky.blackbaud.com",
+      subscriptionKey: process.env.BLACKBAUD_SUBSCRIPTION_KEY
     },
     sync: {
-      studentRole: process.env["BLACKBAUD_STUDENT_ROLE"] || "Student",
-      parentRole: process.env["BLACKBAUD_PARENT_ROLE"] || "Parent"
+      studentRole: process.env.BLACKBAUD_STUDENT_ROLE || "Student",
+      parentRole: process.env.BLACKBAUD_PARENT_ROLE || "Parent"
     }
   },
   google: {
     auth: {
-      serviceEmail: process.env["GOOGLE_AUTH_SERVICE_EMAIL"],
-      serviceKey: process.env["GOOGLE_AUTH_SERVICE_KEY"]
+      serviceEmail: process.env.GOOGLE_AUTH_SERVICE_EMAIL,
+      serviceKey: process.env.GOOGLE_AUTH_SERVICE_KEY
     },
-    domain: process.env["GOOGLE_DOMAIN"],
-    studentGroupEmailPrefix: process.env["GOOGLE_STUDENT_GROUP_EMAIL_PREFIX"] || "students",
-    studentGroupName: process.env["GOOGLE_STUDENT_GROUP_NAME"] || "Class of ",
+    domain: process.env.GOOGLE_DOMAIN,
+    studentGroupEmailPrefix: process.env.GOOGLE_STUDENT_GROUP_EMAIL_PREFIX || "students",
+    studentGroupName: process.env.GOOGLE_STUDENT_GROUP_NAME || "Class of ",
     // See https://developers.google.com/admin-sdk/groups-settings/v1/reference/groups#resource for exact group settings
     studentGroupPermissions: {
       whoCanAdd: "ALL_MANAGERS_CAN_ADD",
@@ -41,8 +43,8 @@ export default {
       whoCanContactOwner: "ALL_MANAGERS_CAN_CONTACT",
       whoCanDiscoverGroup: "ALL_MEMBERS_CAN_DISCOVER"
     },
-    parentGroupEmailPrefix: process.env["GOOGLE_PARENT_GROUP_EMAIL_PREFIX"] || "parents",
-    parentGroupName: process.env["GOOGLE_PARENT_GROUP_NAME"] || "Parents of ",
+    parentGroupEmailPrefix: process.env.GOOGLE_PARENT_GROUP_EMAIL_PREFIX || "parents",
+    parentGroupName: process.env.GOOGLE_PARENT_GROUP_NAME || "Parents of ",
     parentGroupPermissions: {
       whoCanAdd: "ALL_MANAGERS_CAN_ADD",
       whoCanJoin: "INVITED_CAN_JOIN",
